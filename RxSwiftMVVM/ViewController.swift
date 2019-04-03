0935ceaf776f95e5cd7f82e74aec087456492115
soom <dokgi88@gmail.com>
2019-04-04 00:13:33 +0900

    Fix typo of ViewController

--- RxSwiftMVVM/ViewController.swift
+++ RxSwiftMVVM/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        title = getClassName(self)
         view.backgroundColor = .black
         
         let logoView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
