6922e831fdd1414f2729b888659130a21a8a0d29
soom <dokgi88@gmail.com>
2019-04-07 23:14:38 +0900

    Add NavigationController

---
+++ RxSwiftMVVM/NavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  NavigationController.swift
+//  RxSwiftMVVM
+//
+//  Created by soom on 07/04/2019.
+//  Copyright © 2019 cashwalk. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let logoView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        logoView.image = UIImage(named: "Marvel_logo.jpg")
+        logoView.contentMode = .scaleAspectFit
+        
+        navigationBar.tintColor = .white
+        navigationBar.barTintColor = .black
+        navigationBar.topItem?.titleView = logoView
+        navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+
+}
